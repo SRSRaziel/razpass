a1aee61036795b890f14762fcd9b8dc569ed261a
Sullivan Raziel <razielcontato@gmail.com>
2024-03-21 16:39:21 -0300

    Finish changePassword function

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -2,13 +2,18 @@
 const arrUppercaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','W','Y','Z'];
 const arrLowercaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','w','y','z'];
 const arrNumbers = [0,1,2,3,4,5,6,7,8,9];
-const arrSymbols = ['!','@','#','$','%','&','*','=','+','/','.',',',';','-'];
+const arrSymbols = ['!','@','#','$','%','&','*','=','+','/',';','-'];
 
 // My components of index.html
 const increaseBtn = document.getElementById('increaseBtn');
 const decreaseBtn = document.getElementById('decreaseBtn');
 const range = document.getElementById('myRange');
 const output = document.getElementById('output');
+const upperC = document.getElementById('upperC');
+const lowerC = document.getElementById('lowerC');
+const numbers = document.getElementById('numbers');
+const symbols = document.getElementById('symbols');
+const passField = document.getElementById('password');
 
 // Functions
 const increaseValue = () => {
@@ -19,6 +24,8 @@ const increaseValue = () => {
       range.value = currentValueRange + 1;
       output.value = currentValueRange + 1;
     }
+
+    changePassword();
   }
   
 const decreaseValue = () => {
@@ -29,10 +36,79 @@ const decreaseValue = () => {
     range.value = currentValueRange - 1;
     output.value = currentValueRange - 1;
   }
+
+  changePassword();
+}
+
+const changePassword = () => {
+  
+  let password = '';
+  let arrOfChar = [];
+  let countPref = 0;
+  const includeUpperC = upperC.checked;
+  const includeLowerC = lowerC.checked;
+  const includeNumbers = numbers.checked;
+  const includeSymbols = symbols.checked;
+
+  if(includeUpperC){
+    countPref++;
+    arrOfChar = arrOfChar.concat(arrUppercaseLetters);
+  }
+  
+  if(includeLowerC){
+    countPref++;
+    arrOfChar = arrOfChar.concat(arrLowercaseLetters);
+  }
+
+  if(includeNumbers){
+    countPref++;
+    arrOfChar = arrOfChar.concat(arrNumbers);
+  }
+
+  if(includeSymbols){
+    countPref++;
+    arrOfChar = arrOfChar.concat(arrSymbols);
+  }
+
+  if(countPref === 1){
+    if(includeUpperC){
+      upperC.disabled = true;
+    }
+
+    if(includeLowerC){
+      lowerC.disabled = true;
+    }
+
+    if(includeNumbers){
+      numbers.disabled = true;
+    }
+
+    if(includeSymbols){
+      symbols.disabled = true;
+    }
+  } else {
+    upperC.disabled = false;
+    lowerC.disabled = false;
+    numbers.disabled = false;
+    symbols.disabled = false;
+  }
+
+  for(let i = 0; i < output.value; i++){
+    let randomIndex = Math.floor(Math.random() * arrOfChar.length)
+    password += arrOfChar[randomIndex];
+  }
+  
+  passField.value = password;
+
 }
 
 
 // Calling functions with event listeners
 increaseBtn.addEventListener('click', increaseValue);
 decreaseBtn.addEventListener('click', decreaseValue);
-  
+upperC.addEventListener('click', changePassword);
+lowerC.addEventListener('click', changePassword);
+numbers.addEventListener('click', changePassword);
+symbols.addEventListener('click', changePassword);
+range.addEventListener('input', changePassword);
+document.addEventListener('DOMContentLoaded', changePassword);
